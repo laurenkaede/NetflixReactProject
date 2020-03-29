10a64af899c7685f2a916df62d92986d261d535d
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:49:08 +0100

    Updates

---
+++ precache-manifest.d94e4b01e4b75911b803286005ba005d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ac61c8bf736098fd0dde094c7115a8",
+    "url": "/NetflixReactProject/index.html"
+  },
+  {
+    "revision": "7a3db8c2e549cfae1e8a",
+    "url": "/NetflixReactProject/static/css/main.4392f0ab.chunk.css"
+  },
+  {
+    "revision": "f0a50771de18248047a8",
+    "url": "/NetflixReactProject/static/js/2.93b642bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NetflixReactProject/static/js/2.93b642bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3db8c2e549cfae1e8a",
+    "url": "/NetflixReactProject/static/js/main.64b4eba3.chunk.js"
+  },
+  {
+    "revision": "3841a960aab96f76fa6f",
+    "url": "/NetflixReactProject/static/js/runtime-main.23fdf173.js"
+  }
+]);
